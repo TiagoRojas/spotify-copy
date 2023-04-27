043cdd02fd2d62af3ea50b9fb2c79b1e5ef5e0d4
Tiago Rojas <rojastiago04@gmail.com>
2023-04-26 22:36:49 -0300

    [26-04-23] - Redirect uri updated

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,9 +4,10 @@ import {changeMode} from "../store/slice/spotifySlice";
 const scopes =
 	"user-read-email user-read-private user-library-read user-library-modify user-read-playback-state user-modify-playback-state playlist-read-private";
 const client_id = process.env.REACT_APP_CLIENT_ID;
+const redirect_uri = process.env.REACT_APP_SPOTIFY_CALLBACK_HOST;
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&scope=${encodeURIComponent(
 	scopes
-)}&redirect_uri=http://localhost:3000/`;
+)}&redirect_uri=${redirect_uri}`;
 
 export default function Login() {
 	const dispatch = useDispatch();
